92e123798dcbdb0f97739a57f10e8b85e3eddb80
Oliver Nordbjerg <hi@notbjerg.me>
2022-08-01 02:32:43 +0200

    tmp

--- crates/revm/src/interpreter/contract.rs
+++ crates/revm/src/interpreter/contract.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::sync::Arc;
 
 use super::bytecode::{Bytecode, BytecodeLocked};
 use crate::{alloc::vec::Vec, CallContext, Spec};
@@ -114,11 +114,11 @@ pub struct ValidJumpAddress {
     pub first_gas_block: u32,
     /// Rc is used here so that we dont need to copy vector. We can move it to more suitable more accessable structure
     /// without copying underlying vec.
-    pub analysis: Rc<Vec<AnalysisData>>,
+    pub analysis: Arc<Vec<AnalysisData>>,
 }
 
 impl ValidJumpAddress {
-    pub fn new(analysis: Rc<Vec<AnalysisData>>, first_gas_block: u32) -> Self {
+    pub fn new(analysis: Arc<Vec<AnalysisData>>, first_gas_block: u32) -> Self {
         Self {
             analysis,
             first_gas_block,
